b82fffcf8d6d8dfe4ec8a0893083d7ea3e36edaa
carrtesy <tommy.dm.kim@gmail.com>
2023-05-07 17:11:59 +0900

    [20230507] refactoring

--- scripts/train_AnomalyTransformer.sh
+++ scripts/train_AnomalyTransformer.sh
@@ -1,9 +1,9 @@
 # AnomalyTransformer
 export CUDA_VISIBLE_DEVICES=$1
-python train.py dataset=SWaT window_size=100 stride=100 eval_stride=100 batch_size=256 eval_batch_size=256 epochs=3 normalization=None model=AnomalyTransformer model.anomaly_ratio=0.5 &&
-python train.py dataset=WADI window_size=100 stride=100 eval_stride=100 batch_size=256 eval_batch_size=256 epochs=3 normalization=None model=AnomalyTransformer model.anomaly_ratio=0.5 &&
-python train.py dataset=PSM window_size=100 stride=100 eval_stride=100 batch_size=256 eval_batch_size=256 epochs=3  normalization=None model=AnomalyTransformer model.anomaly_ratio=1.0 &&
-python train.py dataset=CreditCard window_size=100 stride=100 eval_stride=100 batch_size=256 eval_batch_size=256 epochs=3 normalization=None model=AnomalyTransformer model.anomaly_ratio=0.5 &&
-python train.py dataset=MSL_P-15 window_size=100 stride=100 eval_stride=100 batch_size=1 eval_batch_size=1 epochs=3 normalization=None model=AnomalyTransformer model.anomaly_ratio=1.0 &&
-python train.py dataset=yahoo_20 window_size=100 stride=100 eval_stride=100 batch_size=1 eval_batch_size=1 epochs=3 normalization=None model=AnomalyTransformer model.anomaly_ratio=0.5 &&
-python train.py dataset=SMD_machine-1-4 window_size=100 stride=100 eval_stride=100 batch_size=1 eval_batch_size=1 epochs=3 normalization=None model=AnomalyTransformer model.anomaly_ratio=0.5
+python train.py dataset=SWaT window_size=100 stride=100 eval_stride=100 batch_size=256 eval_batch_size=256 normalization=None model=AnomalyTransformer model.anomaly_ratio=0.5 &&
+python train.py dataset=WADI window_size=100 stride=100 eval_stride=100 batch_size=256 eval_batch_size=256 normalization=None model=AnomalyTransformer model.anomaly_ratio=0.5 &&
+python train.py dataset=PSM window_size=100 stride=100 eval_stride=100 batch_size=256 eval_batch_size=256 normalization=None model=AnomalyTransformer model.anomaly_ratio=1.0 &&
+python train.py dataset=CreditCard window_size=100 stride=100 eval_stride=100 batch_size=256 eval_batch_size=256 normalization=None model=AnomalyTransformer model.anomaly_ratio=0.5 &&
+python train.py dataset=MSL_P-15 window_size=100 stride=100 eval_stride=100 batch_size=1 eval_batch_size=1 normalization=None model=AnomalyTransformer model.anomaly_ratio=1.0 &&
+python train.py dataset=SMD_machine-1-4 window_size=100 stride=100 eval_stride=100 batch_size=1 eval_batch_size=1 normalization=None model=AnomalyTransformer model.anomaly_ratio=0.5
+python train.py dataset=yahoo_20 window_size=100 stride=100 eval_stride=100 batch_size=1 eval_batch_size=1 normalization=None model=AnomalyTransformer model.anomaly_ratio=0.5 &&
--- scripts/train_USAD.sh
+++ scripts/train_USAD.sh
@@ -1,9 +1,9 @@
 # USAD
 export CUDA_VISIBLE_DEVICES=$1
-python train.py dataset=SWaT model.latent_dim=40 window_size=12 stride=12 batch_size=64 eval_batch_size=64 epochs=30 model=USAD scaler=minmax normalization=None
-python train.py dataset=WADI model.latent_dim=40 window_size=10 stride=10 eval_stride=10 batch_size=64 eval_batch_size=64 epochs=30 model=USAD scaler=minmax normalization=None
-python train.py dataset=PSM model.latent_dim=40 window_size=12 stride=12 eval_stride=12 batch_size=64 eval_batch_size=64 epochs=30 model=USAD scaler=minmax normalization=None
-python train.py dataset=MSL_P-15 model.latent_dim=33 window_size=5 stride=5 eval_stride=5 batch_size=64 eval_batch_size=64 epochs=30 model=USAD scaler=minmax normalization=None
-python train.py dataset=yahoo_20 model.latent_dim=40 window_size=12 stride=12 eval_stride=12 batch_size=2 eval_batch_size=2 epochs=30 model=USAD scaler=minmax normalization=None
-python train.py dataset=CreditCard model.latent_dim=40 window_size=12 stride=12 eval_stride=12 batch_size=64 eval_batch_size=64 epochs=30 model=USAD scaler=minmax normalization=None
-python train.py dataset=SMD_machine-1-4 model.latent_dim=38 window_size=5 stride=5 eval_stride=5 batch_size=64 eval_batch_size=64 epochs=30 model=USAD scaler=minmax normalization=None
+python train.py dataset=SWaT window_size=12 stride=12 eval_stride=12 batch_size=64 eval_batch_size=64 epochs=30 scaler=minmax model=USAD normalization=None model.latent_dim=40
+python train.py dataset=WADI window_size=12 stride=12 eval_stride=12 batch_size=64 eval_batch_size=64 epochs=30 scaler=minmax model=USAD scaler=minmax normalization=None  model.latent_dim=40
+python train.py dataset=PSM window_size=12 stride=12 eval_stride=12 batch_size=64 eval_batch_size=64 epochs=30 scaler=minmax model=USAD scaler=minmax normalization=None model.latent_dim=40
+python train.py dataset=CreditCard window_size=12 stride=12 eval_stride=12 batch_size=64 eval_batch_size=64 epochs=30 scaler=minmax model=USAD scaler=minmax normalization=None model.latent_dim=40
+python train.py dataset=MSL_P-15 window_size=5 stride=5 eval_stride=5 batch_size=8 eval_batch_size=8 epochs=30 scaler=minmax model=USAD scaler=minmax normalization=None model.latent_dim=33
+python train.py dataset=SMD_machine-1-4 window_size=5 stride=5 eval_stride=5 batch_size=8 eval_batch_size=8 epochs=30 scaler=minmax model=USAD scaler=minmax normalization=None model.latent_dim=38
+python train.py dataset=yahoo_20 window_size=5 stride=1 eval_stride=5 batch_size=8 eval_batch_size=8 epochs=30 scaler=minmax model=USAD scaler=minmax normalization=None model.latent_dim=40
