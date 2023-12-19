ec334e4ff8ec39f45bda59ba91c53365244d4db0
carrtesy <tommy.dm.kim@gmail.com>
2023-12-19 06:50:07 +0000

    [20231219] updated model codes

--- models/MLP.py
+++ models/MLP.py
@@ -9,12 +9,11 @@ from models.Normalizer import Detrender
 
 
 class MLP(nn.Module):
-    def __init__(self, seq_len, num_channels, latent_space_size, gamma, normalization="None",
-                 use_sigmoid_output=False, use_dropout=False, use_batchnorm=False):
+    def __init__(self, seq_len, num_channels, latent_space_size, gamma, normalization="None"):
         super().__init__()
         self.L, self.C = seq_len, num_channels
-        self.encoder = Encoder(seq_len*num_channels, latent_space_size, use_dropout, use_batchnorm)
-        self.decoder = Decoder(seq_len*num_channels, latent_space_size, use_dropout, use_batchnorm)
+        self.encoder = Encoder(seq_len*num_channels, latent_space_size)
+        self.decoder = Decoder(seq_len*num_channels, latent_space_size)
         self.normalization = normalization
 
         if self.normalization == "Detrend":
@@ -23,11 +22,6 @@ class MLP(nn.Module):
         else:
             self.use_normalizer = False
 
-        self.use_sigmoid_output = use_sigmoid_output
-        if self.use_sigmoid_output:
-            self.sigmoid = torch.nn.Sigmoid()
-
-
 
     def forward(self, X):
         B, L, C = X.shape
@@ -35,18 +29,17 @@ class MLP(nn.Module):
 
         if self.use_normalizer:
             X = self.normalizer(X, "norm")
+            
         z = self.encoder(X.reshape(B, L*C))
         out = self.decoder(z).reshape(B, L, C)
 
-        if self.use_sigmoid_output:
-            out = self.sigmoid(out)
         if self.use_normalizer:
             out = self.normalizer(out, "denorm")
         return out
 
 
 class Encoder(nn.Module):
-    def __init__(self, input_size, latent_space_size, use_dropout=False, use_batchnorm=False):
+    def __init__(self, input_size, latent_space_size):
         super().__init__()
         self.linear1 = nn.Linear(input_size, input_size // 2)
         self.relu1 = nn.ReLU()
@@ -56,45 +49,18 @@ class Encoder(nn.Module):
         self.relu3 = nn.ReLU()
 
 
-        self.use_dropout = use_dropout
-        if self.use_dropout:
-            self.dropout1 = nn.Dropout(p=0.2)
-            self.dropout2 = nn.Dropout(p=0.2)
-            self.dropout3 = nn.Dropout(p=0.2)
-
-        self.use_batchnorm = use_batchnorm
-        if self.use_batchnorm:
-            self.batchnorm1 = nn.BatchNorm1d(input_size//2)
-            self.batchnorm2 = nn.BatchNorm1d(input_size//4)
-            self.batchnorm3 = nn.BatchNorm1d(latent_space_size)
-
-
     def forward(self, x):
         x = self.linear1(x)
-        if self.use_batchnorm:
-            x = self.batchnorm1(x)
         x = self.relu1(x)
-        if self.use_dropout:
-            x = self.dropout1(x)
-
         x = self.linear2(x)
-        if self.use_batchnorm:
-            x = self.batchnorm2(x)
         x = self.relu2(x)
-        if self.use_dropout:
-            x = self.dropout2(x)
-
         x = self.linear3(x)
-        if self.use_batchnorm:
-            x = self.batchnorm3(x)
         x = self.relu3(x)
-        if self.use_dropout:
-            x = self.dropout3(x)
         return x
 
 
 class Decoder(nn.Module):
-    def __init__(self, input_size, latent_space_size, use_dropout=False, use_batchnorm=False):
+    def __init__(self, input_size, latent_space_size):
         super().__init__()
         self.linear1 = nn.Linear(latent_space_size, input_size // 4)
         self.relu1 = nn.ReLU()
@@ -102,32 +68,12 @@ class Decoder(nn.Module):
         self.relu2 = nn.ReLU()
         self.linear3 = nn.Linear(input_size // 2, input_size)
 
-        self.use_dropout = use_dropout
-        if self.use_dropout:
-            self.dropout1 = nn.Dropout(p=0.2)
-            self.dropout2 = nn.Dropout(p=0.2)
-            self.dropout3 = nn.Dropout(p=0.2)
-
-        self.use_batchnorm = use_batchnorm
-        if self.use_batchnorm:
-            self.batchnorm1 = nn.BatchNorm1d(input_size//4)
-            self.batchnorm2 = nn.BatchNorm1d(input_size//2)
 
     def forward(self, x):
         x = self.linear1(x)
-        if self.use_batchnorm:
-            x = self.batchnorm1(x)
         x = self.relu1(x)
-        if self.use_dropout:
-            x = self.dropout1(x)
-
         x = self.linear2(x)
-        if self.use_batchnorm:
-            x = self.batchnorm2(x)
         x = self.relu2(x)
-        if self.use_dropout:
-            x = self.dropout2(x)
-
         out = self.linear3(x)
         return out
 
